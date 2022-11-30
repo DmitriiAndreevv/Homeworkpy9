bccd5f786b0e770027ae71cee15ee05f43449311
dmitriy.andreev.1995@inbox.ru <dmitriy.andreev.1995@inbox.ru>
2022-11-30 22:40:49 +0300

    Main

---
+++ TicTacToemain.py
@@ -0,0 +1,16 @@
+from TicTacToe import Tic_Tac_Toe
+
+board = Tic_Tac_Toe()
+print(*board.get_field(), sep="\n")
+print(board.make_move(1, 1))
+print(*board.get_field(), sep="\n")
+print(board.make_move(1, 1))
+print(board.make_move(1, 2))
+print(*board.get_field(), sep="\n")
+print(board.make_move(2, 1))
+print(board.make_move(2, 2))
+print(*board.get_field(), sep="\n")
+print(board.make_move(3, 1))
+print(*board.get_field(), sep="\n")
+print(board.make_move(2, 2))
+print(*board.get_field(), sep="\n")
